494501d98442bea82e1a4507805467be95050a6d
Maanav Garg <maanavgarg@icloud.com>
2019-07-16 15:11:31 -0400

    defined ticket type and country

--- main.py
+++ main.py
@@ -21,3 +21,22 @@ return_ticket = "//label[@id='flight-type-roundtrip-label-hp-flight']"
 one_way_ticket = "//label[@id='flight-type-one-way-label-hp-flight']"
 multi_ticket = "//label[@id='flight-type-multi-dest-label-hp-flight']"
 
+# function that chooses a ticket type 
+def ticket_chooser(ticket):
+    try:
+        ticket_type = browser.find_element_by_xpath(ticket)
+        ticket_type.click()
+    except Exception as e:
+        pass
+
+# function that chooses departure country
+def dep_country_chooser(dep_country):
+    fly_from = browser.find_element_by_xpath("//input[@id='flight-origin-hp-flight']")
+    time.sleep(1)
+    fly_from.clear()
+    time.sleep(1.5)
+    fly_from.send_keys('  ' + dep_country)
+    time.sleep(1.5)
+    first_item = browser.find_element_by_xpath("//a[@id='aria-option-0']")
+    time.sleep(1.5)
+    first_item.click()
